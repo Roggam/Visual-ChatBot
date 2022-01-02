88f17f9f460f6940f8cb08f4d3c547baae4e9dc9
Keerthana Srinivasan <keerthihamsa.06@gmail.com>
2022-01-02 17:54:23 -0500

    update visual api and ui alignment

--- visual-chatbot/src/test/java/com/zipcode/java/project/visual/chatbot/VisualChatbotApplicationTests.java
+++ visual-chatbot/src/test/java/com/zipcode/java/project/visual/chatbot/VisualChatbotApplicationTests.java
@@ -1,13 +1,13 @@
-package com.zipcode.java.project.visual.chatbot;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class VisualChatbotApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.zipcode.java.project.visual.chatbot;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class VisualChatbotApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
